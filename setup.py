1578617805ac10e14f59c39a9300f045d28d1d7d
LouisRub <louis.rubeus@edu.devinci.fr>
2024-01-29 14:11:27 +0100

    Creatation of setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+ name='Game-of-life',
+ version='1.0',
+ author='LouisRub',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
